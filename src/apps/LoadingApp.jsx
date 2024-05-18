7a4fb9b778077c81bb4e6f6adf55fc5d8b53e752
csantacruz1127 <csantacruz1127@gmail.com>
2024-05-18 23:41:36 +0000

    app loading

---
+++ src/apps/LoadingApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './LoadingApp.scss'
+
+const LoadingApp = () => {
+  return (
+    <div className='loading-screen'>
+        <div className='loading-screen__body'>
+            <div className='loading-circle'>
+
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default LoadingApp;
--- src/components/tasks/FormTask.jsx
+++ src/components/tasks/FormTask.jsx
@@ -9,6 +9,7 @@ import { GlobalContext } from "../../context/UserProvider";
 import { useState, useContext } from "react";
 
 //local
+import LoadingApp from '../../apps/LoadingApp';
 import BaseForm from "../base/BaseForm";
 import BaseSelectForm from "../base/BaseSelectForm";
 // services
@@ -16,8 +17,8 @@ import { apiAddTask } from '../../services/TaskServices.js'
 
 
 function FormTask() {
+  const [load, setLoad] = useState(false) 
   const { user } = useContext(GlobalContext);
-  
   const [titleTask, setTitleTask] = useState('')
   const [descriptionTask, setdescriptionTask] = useState('')
   const [dateTask, setdateTask] = useState('')
@@ -31,8 +32,8 @@ function FormTask() {
   ]
 
   const enviarDatos = async () => {
+    setLoad(true)
     const ahora = format(new Date(), 'dd-MM-yyyy')
-    console.log(titleTask, descriptionTask, dateTask, sizeTask, user.id, ahora);
     let data = {
       created: ahora,
       title: titleTask,
@@ -43,52 +44,57 @@ function FormTask() {
       state: 'En Proceso'
     }
     try {
-      const response = apiAddTask(data)
-      console.log(response);
+      const response = await apiAddTask(data)
+      setLoad(false)
+      console.log(response.id);
     } catch (error) {
+      setLoad(false)
       console.log(error)
     }
   }
 
-  return ( 
-    <form className="">
-      <BaseForm
-        tipo="text"
-        placeholder="Tarea..." 
-        icon={<BiTask />}
-        value={titleTask}
-        onChange={(value) => {setTitleTask(value)}}
-      />
-      <BaseForm
-        tipo="text"
-        placeholder="Descripcion..."
-        icon={<MdOutlineDescription />} 
-        value={descriptionTask}
-        onChange={(value) => {setdescriptionTask(value)}}
-      />
-      <BaseForm
-        tipo="date"
-        placeholder="Descripcion..." 
-        icon={<MdDateRange />}
-        value={dateTask}
-        onChange={(value) => {setdateTask(value)}}
-      />
-      <label className="label">Tamaño:</label>
-      <BaseSelectForm 
-        options={optionsSizeTask}
-        value={sizeTask}
-        onChange={(value) => {setsizeTask(value)}}
-      />
-      <div className="is-flex is-justify-content-center  p-2 mt-6">
-        <button 
-          type="button"
-          onClick={enviarDatos}
-          className="button is-info is-rounded is-fullwidth"
-        >
-            Agregar Tarea
-        </button>
-      </div>
-    </form>
+  return (
+    <>
+      { load ? <LoadingApp /> : null } 
+      <form className="">
+        <BaseForm
+          tipo="text"
+          placeholder="Tarea..." 
+          icon={<BiTask />}
+          value={titleTask}
+          onChange={(value) => {setTitleTask(value)}}
+        />
+        <BaseForm
+          tipo="text"
+          placeholder="Descripcion..."
+          icon={<MdOutlineDescription />} 
+          value={descriptionTask}
+          onChange={(value) => {setdescriptionTask(value)}}
+        />
+        <BaseForm
+          tipo="date"
+          placeholder="Descripcion..." 
+          icon={<MdDateRange />}
+          value={dateTask}
+          onChange={(value) => {setdateTask(value)}}
+        />
+        <label className="label">Tamaño:</label>
+        <BaseSelectForm 
+          options={optionsSizeTask}
+          value={sizeTask}
+          onChange={(value) => {setsizeTask(value)}}
+        />
+        <div className="is-flex is-justify-content-center  p-2 mt-6">
+          <button 
+            type="button"
+            onClick={enviarDatos}
+            className="button is-info is-rounded is-fullwidth"
+          >
+              Agregar Tarea
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
